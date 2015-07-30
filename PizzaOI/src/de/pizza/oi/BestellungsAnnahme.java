075bcb840c07b4322830ac3147bed1d2220d7e92
tglaser <tglaser@it-economics.de>
2015-07-30 20:21:14 +0200

    Pizza Margarita

--- PizzaOI/src/de/pizza/oi/BestellungsAnnahme.java
+++ PizzaOI/src/de/pizza/oi/BestellungsAnnahme.java
@@ -3,7 +3,11 @@ package de.pizza.oi;
 public class BestellungsAnnahme {
 
 	public void bestelle(String bestellung, Tisch tisch) {
-		tisch.liefere(new Pizzabrot());
+		if (bestellung.equals("Pizza Margarita")) {
+			tisch.liefere(new Pizza("Tomate", "Käse"));
+		} else {
+			tisch.liefere(new Pizzabrot());
+		}
 		
 	}
 
--- PizzaOI/src/de/pizza/oi/PizzaBestellTest.java
+++ PizzaOI/src/de/pizza/oi/PizzaBestellTest.java
@@ -1,18 +1,34 @@
 package de.pizza.oi;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.core.Is.isA;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 
 public class PizzaBestellTest {
 
+	private HolzTisch tisch = new HolzTisch();
+
 	@Test
-	public void kannFertigesPizzabrotBestellen() {
-		BestellungsAnnahme bestellungsAnnahme = new BestellungsAnnahme();
-		HolzTisch tisch = new HolzTisch();
-		bestellungsAnnahme.bestelle("Pizzabrot", tisch);
+	public void kannPizzabrotBestellen() {
+		bestelle("Pizzabrot");
 		assertThat(tisch.wasLiegtAufDemTisch(), isA(Pizzabrot.class));
+	}
+	
+	@Test
+	public void kannPizzaMargaritaBestellen() throws Exception {
+		bestelle("Pizza Margarita");
+		Pizza pizza = (Pizza)tisch.wasLiegtAufDemTisch();
+		assertThat(pizza, isA(Pizza.class));
+		assertThat(pizza.getZutaten(), hasItems("Tomate", "Käse"));
+	}
+
+	private void bestelle(String bestellung) {
+		BestellungsAnnahme bestellungsAnnahme = new BestellungsAnnahme();
+		bestellungsAnnahme.bestelle(bestellung, tisch);
 	}
 
 	private static class HolzTisch implements Tisch {
